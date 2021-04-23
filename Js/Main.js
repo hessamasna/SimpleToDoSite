b711748c172d7e503720a240b5edf91116f61c46
hessamasna <hessamasna@yahoo.com>
2021-04-23 21:17:23 +0430

    start Js

---
+++ Js/Main.js
@@ -0,0 +1,12 @@
+function plus() {
+    var addNewTaskText = document.getElementById("task-text").value;
+    var newTaskDate = document.getElementById("dateAndTime").value;
+    //2021-04-19T21:08
+    if (newTaskDate === "") {
+    }
+    console.log(addNewTaskText);
+}
+function bookmark() { }
+//   function makeTaskHtml(task){
+//     //   let li = `<div class="task-line"><div style="display: flex; align-items: center;"><button id="check"><i class="far fa-circle"></i></button><div style="font-size:  1.2vmax; margin-left: 10px;">${task}</div></div><div><div style="display: flex; align-items: center;"><div style="margin-right: 20px; font-size:  1.2vmax;">Wednesday, April 20 23:10</div><button id="bookmark"><i class="far fa-bookmark"></i></button></div></div></div>`
+//   }
---
+++ Js/Main.ts
@@ -0,0 +1,14 @@
+function plus() {
+  let addNewTaskText = document.getElementById("task-text").value;
+  let newTaskDate = document.getElementById("dateAndTime").value;
+
+  if (newTaskDate === "") {
+  }
+  console.log(addNewTaskText);
+}
+
+function bookmark() {}
+
+//   function makeTaskHtml(task){
+//     //   let li = `<div class="task-line"><div style="display: flex; align-items: center;"><button id="check"><i class="far fa-circle"></i></button><div style="font-size:  1.2vmax; margin-left: 10px;">${task}</div></div><div><div style="display: flex; align-items: center;"><div style="margin-right: 20px; font-size:  1.2vmax;">Wednesday, April 20 23:10</div><button id="bookmark"><i class="far fa-bookmark"></i></button></div></div></div>`
+//   }
