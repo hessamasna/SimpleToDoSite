40866b930b18edf74f21f5f1db06ac89c947e767
hessamasna <hessamasna@yahoo.com>
2021-04-24 23:00:05 +0430

    work on convert of date

--- Js/Main.js
+++ Js/Main.js
@@ -1,10 +1,73 @@
+function findmonth(count) {
+    var months = [
+        "December",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+    ];
+    return months[count];
+}
+function findWeekDay(count) {
+    var days = [
+        "Friday",
+        "Saturday",
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+    ];
+    return days[count];
+}
+var alltasks = [
+    {
+        isDone: false,
+        taskText: "test",
+        time: "2020/12/12 13:20",
+        dateSplit: {
+            year: 0,
+            month: 0,
+            day: 0,
+            hour: 0,
+            min: 0
+        },
+        isBookmark: false
+    },
+];
 function plus() {
     var addNewTaskText = document.getElementById("task-text").value;
-    var newTaskDate = document.getElementById("dateAndTime").value;
-    //2021-04-19T21:08
-    if (newTaskDate === "") {
+    var newTaskDateAndTime = document.getElementById("dateAndTime").value;
+    var isDone, taskText, time, isBookmark, dateSplit;
+    if (newTaskDateAndTime === "") {
+        time = "not set";
+        dateSplit = {
+            year: 0,
+            month: 0,
+            day: 0,
+            hour: 0,
+            min: 0
+        };
     }
-    console.log(addNewTaskText);
+    else {
+        var taskDate = newTaskDateAndTime.split("T");
+        var date = new Date(taskDate[0].split("-")[0], taskDate[0].split("-")[1], taskDate[0].split("-")[2], taskDate[1].split(":")[0], taskDate[1].split(":")[1]);
+        time = findWeekDay(date.getDay()) + ", " + findmonth(date.getMonth()) + " " + taskDate[0].split("-")[2] + " " + taskDate[1].split(":")[0] + ":" + taskDate[1].split(":")[1];
+        dateSplit = {
+            year: taskDate[0].split("-")[0],
+            month: taskDate[0].split("-")[1],
+            day: taskDate[0].split("-")[2],
+            hour: taskDate[1].split(":")[0],
+            min: taskDate[1].split(":")[1]
+        };
+    }
 }
 function bookmark() { }
 //   function makeTaskHtml(task){
--- Js/Main.ts
+++ Js/Main.ts
@@ -1,10 +1,87 @@
+function findmonth(count) {
+  var months = [
+    "December",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+  ];
+  return months[count];
+}
+
+function findWeekDay(count) {
+  var days = [
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+  ];
+  return days[count];
+}
+
+var alltasks = [
+  {
+    isDone: false,
+    taskText: "test",
+    time: "2020/12/12 13:20",
+    dateSplit: {
+      year: 0,
+      month: 0,
+      day: 0,
+      hour: 0,
+      min: 0,
+    },
+    isBookmark: false,
+  },
+];
+
 function plus() {
   let addNewTaskText = document.getElementById("task-text").value;
-  let newTaskDate = document.getElementById("dateAndTime").value;
+  let newTaskDateAndTime = document.getElementById("dateAndTime").value;
+  let isDone, taskText, time, isBookmark, dateSplit;
 
-  if (newTaskDate === "") {
+  if (newTaskDateAndTime === "") {
+    time = "not set";
+    dateSplit = {
+      year: 0,
+      month: 0,
+      day: 0,
+      hour: 0,
+      min: 0,
+    };
+  } else {
+    let taskDate = newTaskDateAndTime.split("T");
+
+    let date = new Date(
+      taskDate[0].split("-")[0],
+      taskDate[0].split("-")[1],
+      taskDate[0].split("-")[2],
+      taskDate[1].split(":")[0],
+      taskDate[1].split(":")[1]
+    );
+
+    time = `${findWeekDay(date.getDay())}, ${findmonth(date.getMonth())} ${
+      taskDate[0].split("-")[2]
+    } ${taskDate[1].split(":")[0]}:${taskDate[1].split(":")[1]}`;
+
+    dateSplit = {
+      year: taskDate[0].split("-")[0],
+      month: taskDate[0].split("-")[1],
+      day: taskDate[0].split("-")[2],
+      hour: taskDate[1].split(":")[0],
+      min: taskDate[1].split(":")[1],
+    };
   }
-  console.log(addNewTaskText);
 }
 
 function bookmark() {}
